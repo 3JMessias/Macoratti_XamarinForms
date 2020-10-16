d529ce29e667865a100fcf0eac9e453f1e9d1e57
Johny Messias <jmessias@startis.com>
2020-10-16 18:20:28 -0300

    [ADD] Concluindo exercicios de 'Modulo 02 - StackLayouts'

---
+++ XamarinForms_Macoratti/XamarinForms_Macoratti/Views/Modulo_02-Layouts/StackLayout_Exercicios/Exercicio03Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinForms_Macoratti.Views.Modulo_02_Layouts.StackLayout_Exercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercicio03Page : ContentPage
+    {
+        public Exercicio03Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
