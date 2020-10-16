b5c283404c086bef505722f1db5bf8a2cf46d1b6
Johny Messias <jmessias@startis.com>
2020-10-16 11:39:29 -0300

    [ADD] Modulo 02 - Iniciando o Exercicio

---
+++ XamarinForms_Macoratti/XamarinForms_Macoratti/ImageResourceExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace XamarinForms_Macoratti
+{
+    [ContentProperty (nameof(Source))]
+    class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source is null) return null;
+
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
---
+++ XamarinForms_Macoratti/XamarinForms_Macoratti/Views/Modulo_02-Layouts/StackLayoutPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinForms_Macoratti
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackLayoutPage : ContentPage
+    {
+        private string likes = "25";
+        
+
+        public string Likes
+        {
+            get { return likes; }
+            set { likes = value; }
+        }
+
+        public StackLayoutPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
