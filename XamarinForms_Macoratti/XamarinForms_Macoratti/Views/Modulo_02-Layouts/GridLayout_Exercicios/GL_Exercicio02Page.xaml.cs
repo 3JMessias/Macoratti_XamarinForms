69a435e2b3b8b64ed67e3ca21c89ed867622cf6e
Johny Messias <jmessias@startis.com>
2020-10-16 18:22:19 -0300

    [ADD] Exercicio 01 de 'Modulo 02 - GridLayouts'

---
+++ XamarinForms_Macoratti/XamarinForms_Macoratti/Views/Modulo_02-Layouts/GridLayout_Exercicios/GL_Exercicio02Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinForms_Macoratti.Views.Modulo_02_Layouts.GridLayout_Exercicios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GL_Exercicio02Page : ContentPage
+    {
+        public GL_Exercicio02Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
